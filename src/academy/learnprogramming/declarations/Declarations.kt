415164ef5d4684d3c697ce2b4603bdf8b4668035
Robert Lowe <robertmlowe@rmlowe.com>
2018-01-21 01:14:57 +0800

    Smart casting

--- src/academy/learnprogramming/declarations/Declarations.kt
+++ src/academy/learnprogramming/declarations/Declarations.kt
@@ -3,9 +3,40 @@ package academy.learnprogramming.declarations
 typealias EmployeeSet = Set<Employee>
 
 fun main(args: Array<String>) {
+
+    val employeeOne = Employee("Mary", 1)
+    val employeeTwo = Employee("John", 2)
+    val employeeThree = Employee("John", 2)
+    println(employeeOne === employeeTwo)
+    println(employeeTwo === employeeThree)
+    println(employeeOne == employeeTwo)
+    println(employeeTwo == employeeThree)
+    val employeeFour = employeeTwo
+    println(employeeFour === employeeTwo)
+
+    println(employeeFour != employeeTwo)
+    println(employeeFour !== employeeTwo)
+    println(employeeTwo != employeeThree)
+    println(employeeTwo !== employeeThree)
+
+
+    val x = 0x00101101
+    val y = 0x11011011
+
+    var something: Any = employeeFour
+    if (something is Employee) {
+        //val newEmployee = something as Employee
+        something = employeeOne
+        println(something.name)
+    }
+
+
     var number: Int
-    number = 10
+    number = 10;
     number = 20
+
+    val names = arrayListOf("John", "Jane", "Mary")
+    println(names[1])
 
     val employees: EmployeeSet
 
@@ -22,12 +53,16 @@ fun main(args: Array<String>) {
         employee2 = Employee("Mike Watson", 150)
     }
 
-
-
-
-
 }
 
 class Employee(var name: String, val id : Int) {
+
+    override fun equals(obj: Any?): Boolean {
+        if (obj is Employee) {
+            return name == obj.name && id == obj.id
+        }
+
+        return false
+    }
 
 }
